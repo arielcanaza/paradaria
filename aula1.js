d253c5ed61a8101253a597ca7b0cbe82f961831d
ariel martinez <canaza23.ac@outlook.com>
2024-05-23 20:13:38 -0300

    padaria com js
    js crip

---
+++ aula1.js
@@ -0,0 +1,24 @@
+let num1 = 133;
+let num2 = 45;
+
+console.log(num1 + num2);
+
+const result = num1 + num2;
+console.log(result);
+
+
+
+const sub = num1 - num2;
+console.log(sub);
+
+const div = num1 / num2;
+console.log(div);
+
+const mult = num1 * num2;
+console.log(mult);
+
+num1 = 33; //aqui por ser tipo LET podemos alterar  seu valor
+num2 = 222; //alteramos o valor
+
+console.log(num1 > num2);
+console.log(num2 > num1);
---
+++ inicio.js
@@ -0,0 +1,16 @@
+const cartao1 = {
+    nome: 'ariel martinez',
+    saldo: 20000,
+    banco: 'Banco Brasil'
+};
+
+const cartao2 = cartao1;
+
+console.log(cartao1);
+console.log(cartao2);
+
+cartao2.saldo = 10;
+console,log(cartao2);
+
+
+//  crie tres tipos diferentes de variaveis
